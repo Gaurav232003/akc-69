8cb0c495e1750c0c233fe11fbf0449c46d945146
huloiarnata <rk710.nr@gmail.com>
2022-09-04 12:40:07 +0530

    backend services

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class SarvSwasthya extends StatefulWidget {
 class _SarvSwasthyaState extends State<SarvSwasthya> {
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return MultiProvider(child: Start(), providers: AppProviders.providers);
   }
 }
@@ -34,11 +33,8 @@ class Start extends StatefulWidget {
 class _StartState extends State<Start> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-=======
     return MaterialApp(
       theme: ThemeData(fontFamily: 'Proxima Nova'),
->>>>>>> 39848c441a2a67b5f9f940244a4ea27f6c28d6ae
       debugShowCheckedModeBanner: false,
       home: Splash(),
     );
